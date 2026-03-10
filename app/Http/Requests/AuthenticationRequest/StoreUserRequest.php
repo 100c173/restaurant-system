760009f6f86ccb9a2b427829ade73b733fa608c0
ameroniz <ameroniza@gmail.com>
2026-03-10 14:31:32 +0300

    fix

--- app/Http/Requests/AuthenticationRequest/StoreUserRequest.php
+++ app/Http/Requests/AuthenticationRequest/StoreUserRequest.php
@@ -39,6 +39,7 @@ class StoreUserRequest extends FormRequest
             ],
 
             'phone' => [
+                'nullable',
                 'string',
                 'regex:/^\+963\s?9\d{2}\s?\d{3}\s?\d{3}$/',
             ],
