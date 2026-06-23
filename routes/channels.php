dd5112346bfa1d04744dece9bc24c8bd83d4aca2
ameroniz <ameroniza@gmail.com>
2026-06-23 16:43:49 +0300

    fix

--- routes/channels.php
+++ routes/channels.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Broadcast;
 use Modules\Orders\Models\TenantOrder;
 
+
 Broadcast::channel('orders.{orderId}', function ($user, $orderId) {
     $order = TenantOrder::find($orderId);
 
-    return $order && ($order->user_id);
+    return $order && $order->user_id === $user->id;
 });
